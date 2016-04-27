5d120d31741e7028caa22ff4ab4975b6b622289b
Hannah Kerner <hannah.r.kerner@gmail.com>
2016-04-27 13:39:22 -0700

    Save session every 10 min

--- v2/trainer/base.py
+++ v2/trainer/base.py
@@ -1,6 +1,6 @@
 
 import collections
-
+import time
 
 class Trainer(object):
 
@@ -14,6 +14,7 @@ class Trainer(object):
 
         self.len_history = len_history
         self.stats_history = collections.deque([], self.len_history)
+        self.last_saved = time.time()
 
     def params_from_stats_history(self, stats_history):
         raise NotImplementedError
@@ -31,6 +32,10 @@ class Trainer(object):
         try:
             while True:
                 self.train_step(learner)
+                cur_time = time.time()
+                if cur_time - self.last_saved > 600:
+                    learner.saver.save(learner.sess, 'saved_sessions/saved_%d' % cur_time)
+                    self.last_saved = cur_time
         except StopIteration:
             return
 
--- v2/trainer/mcam1.py
+++ v2/trainer/mcam1.py
@@ -13,15 +13,16 @@ class McamTrainer(trainer.base.Trainer):
     def __init__(self, dataset, len_history=10, xent_threshold=1, init_compression=1):
         self.xent_threshold = xent_threshold
         self.init_compression = init_compression
-        self.max_compression = init_compression
+        self.max_compression = 95
         super(self.__class__, self).__init__(dataset, len_history=len_history)
 
     def params_from_stats_history(self, stats_history):
-        if len(stats_history) < self.len_history:
+        if False:
+        #if len(stats_history) < self.len_history:
             compression = self.init_compression
         else:
             mean_xent = np.mean(stats_history)
             if mean_xent < self.xent_threshold and self.max_compression < 95:
-                self.max_compression += 10
+                self.max_compression += 1
             compression = random.randint(self.init_compression, self.max_compression)
         return {'compression': compression}
